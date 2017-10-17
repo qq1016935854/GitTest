3c5b504db3c4d0c47970bcbe4d3b6c9582ae8d6c
qq1016935854 <57320673@qq.com>
2017-10-17 11:21:42 +0800

    Revert "项目"
    This reverts commit 07c2357ce963d3cdf14f16cb17fce526c5a9bee6.

--- Purejoy/src/com/itqf/dao/FitnessfoodDao.java
+++
@@ -1,9 +0,0 @@
-package com.itqf.dao;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-
-public interface FitnessfoodDao {
-
-	String getAllFitnessfood();
-
-}
--- Purejoy/src/com/itqf/service/FitnessfoodService.java
+++
@@ -1,9 +0,0 @@
-package com.itqf.service;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-
-public interface FitnessfoodService {
-
-	String getAllFitnessfood();
-
-}
